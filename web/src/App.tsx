142a30984dc67ba74dfaefc8e874d25a11a8243d
Allison Piovani <apiovani25@gmail.com>
2020-08-07 19:48:26 -0300

    Add routes to project

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-import Landing from "./pages/Landing";
-
 import "./assets/styles/global.css";
+import Routes from "./routes";
 
 function App() {
-  return <Landing />;
+  return (
+      <Routes />
+  )
 }
 
 export default App;
