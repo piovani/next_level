f8c26be532c5931d4d131fc75c503a9ca4129fb4
Allison Piovani <apiovani25@gmail.com>
2020-08-08 14:31:20 -0300

    styles page teacher list finished

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import backIcon from '../../assets/images/icons/back.svg';
+import logoIcon from '../../assets/images/logo.svg';
+
+import './styles.css'
 
 function TeacherList() {
     return (
-        <h1>Teachers</h1>
+        <div id="page-teacher-list" className="container">
+            <header className="page-header">
+                <div className="top-bar-container">
+                    <Link to="/">
+                        <img src={backIcon} alt="Voltar"/>
+                    </Link>
+                    
+                    <img src={logoIcon} alt="Proffy" />
+                </div>
+
+                <div className="header-content">
+                    <strong>Estes são os proffys disponíveis.</strong>
+                </div>
+            </header>
+        </div>
     )
 }
 
