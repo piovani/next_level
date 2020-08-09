384c74a2983594ee61c6f03e03d6bc7bc80f04e1
Allison Piovani <apiovani25@gmail.com>
2020-08-09 00:19:00 -0300

    Route working with try and catch

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,71 @@
-import express from 'express';
+import express, { response, response } from 'express';
+import db from './database/connection';
+import convertToHourToMinutes from '../utils/convertHourToMinites';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({message: 'Hello World'});
+interface ScheduleItem {
+    week_day: number;
+    from: string;
+    to: string;
+}
+
+routes.post('/classes', async (request, response) => {
+    const {
+        name,
+        avatar,
+        whatsapp,
+        bio,
+        subject,
+        cost,
+        schedule
+    } = request.body;
+    
+    const trx = await db.transaction();
+
+    try {
+        const insertUsersIds = await trx('users').insert({
+            name,
+            avatar,
+            whatsapp,
+            bio,
+        });
+
+        const user_id = insertUsersIds[0];
+
+        const insertClassesIds = await trx('classes').insert({
+            subject,
+            cost,
+            user_id,
+        });
+
+        const class_id = insertClassesIds[0];
+
+        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+            return { 
+                class_id,
+                week_day: scheduleItem.week_day,
+                from: convertToHourToMinutes(scheduleItem.from),
+                to: convertToHourToMinutes(scheduleItem.to)
+            }
+        })
+
+        await trx('class_schedule').insert(classSchedule);
+
+        await trx.commit();
+
+        return response.status(201).send();
+
+    } catch (exeception) {
+
+        trx.rollback();
+
+        console.log(exeception);
+
+        return response.status(400).json({
+            error: 'Unexpected error while creating new class',
+        });
+    }
 });
 
 export default routes;
